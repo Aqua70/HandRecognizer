6aa5235293d9bfe38737f2514bf25b208e64e4f2
truanov1 <ilancito7@gmail.com>
2020-12-25 12:21:41 -0500

    Data set for gesture recognition

--- cameraDetection.py
+++ cameraDetection.py
@@ -2,16 +2,20 @@
 import numpy as np
 import cv2
 cap = cv2.VideoCapture(0)
-
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    bottomLeft = (125, 20);
+    topRight = (525,420);
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, 0)
-
+    gray = cv2.cvtColor(frame, 10)
+    # rect = cv2.rectangle(gray, bottomLeft, topRight, (255,0,0), 2)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(gray, (x,y), (x+w,y+h), (255,0,0), 2)
+    cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
